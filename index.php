b506e9836f8cc2705d8fbf7fedfc39cf9e1fb0d1
lestercoyoyjr <lester_cch@hotmail.com>
2022-05-01 22:48:55 -0600

    Upload & Save
    Uploading images with $_FILES and keeping the quality with imagejpeg but we could use png, bit, gif, etc. But we must change the extension

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h2>Redimensionar Imagenes</h2>
 
-    <form id="form" name="forma" action="procesa.php" method="POST" enctype="multipart/form-data"></form>
+    <form id="form" name="forma" action="procesa.php" method="post" enctype="multipart/form-data">
 
-    <p>Imagen: </p>
+        <p>Imagen: </p>
 
-    <input type="file" id="imagen" name="imagen" accept="image/jpeg" required>
+        <input type="file" id="imagen" name="imagen" accept="image/jpeg" required>
 
-    <p>
-        <button type="submit">Cargar</button>
-    </p>
+        <p>
+            <button type="submit">Cargar</button>
+        </p>
+    </form>
+
 </body>
+
 </html>
---
+++ procesa.php
@@ -0,0 +1,22 @@
+<?php
+    $destino = "img/foto.jpg";
+
+    $nAncho = 400;
+    $nAlto = 400;
+
+    if ($_FILES['imagen']['error'] === UPLOAD_ERR_OK) {
+
+        $imagen_original = $_FILES['imagen']['tmp_name'];
+
+        $img_original = imagecreatefromjpeg($imagen_original);
+        $ancho_original = imagesx($img_original);
+        $alto_original = imagesy($img_original);
+
+        $tmp = imagecreatetruecolor($nAncho, $nAlto);
+
+        imagecopyresized($tmp, $img_original, 0, 0, 0, 0, $nAncho, $nAlto, $ancho_original, $alto_original);
+
+        imagejpeg($tmp, $destino, 100);
+
+    }
+?>
